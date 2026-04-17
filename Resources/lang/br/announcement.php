0274c10dba1fa01d5481a2fd71b60aa7cdbf6362
crashzk <zkservidores@gmail.com>
2026-04-17 19:45:28 -0300

    Update announcement.php

--- Resources/lang/br/announcement.php
+++ Resources/lang/br/announcement.php
@@ -2,5 +2,16 @@
 
 return [
     'module_name' => 'Anúncios',
-    'description' => 'Módulo para exibir anúncios no topo do site',
+    'description' => 'Módulo para anúncios no topo do site',
+    'timer' => [
+        'days_short' => 'd',
+        'hours_short' => 'h',
+        'minutes_short' => 'm',
+        'seconds_short' => 's',
+        'days' => 'Dias',
+        'hours' => 'Horas',
+        'minutes' => 'Minutos',
+        'seconds' => 'Segundos',
+        'expired' => 'Expirado',
+    ],
 ];
